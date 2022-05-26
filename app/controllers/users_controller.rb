6d53c162ad9bb650b405931ee6bc2523ac4e1847
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-05-25 21:58:17 -0400

    commit tree 8175883d58038e13d5548e8becede6db84c96b1a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
 
     def create 
+        # byebug
         user = User.create(user_params)
         if user.valid?
+            session[:user_id] = user.id
             render json: user, status: :created
         else
             render json: { errros: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show 
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user 
+        else
+            render json: { error: "Not authorized" }, status: :unauthorized
         end
     end
 
